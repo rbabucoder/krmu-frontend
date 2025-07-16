2ea5d82f07313d8bf9eaf818573a1cb20ffd3f77
rbabucoder <baburohit0131@gmail.com>
2025-07-16 12:42:08 +0530

    make Accrediation, Recognition and Approval Page and also make it dynamic and fix design and responsive of member and ranking page

--- app/(krmu-about)/membership-and-rankings/page.tsx
+++ app/(krmu-about)/membership-and-rankings/page.tsx
@@ -9,7 +9,7 @@ const page = async () => {
 
   const members = membershipRanking?.memberships;
 
-  return (
+  return ( 
     <>
       <section className="pt-40 pb-10 px-4 membership-ranking-bg">
         <div className="max-w-[1600px] mx-auto w-full">
@@ -19,23 +19,24 @@ const page = async () => {
             </h3>
             <p>{membershipRanking?.description}</p>
           </div>
-          <div className="grid grid-col-1 md:grid-cols-2  lg:grid-cols-3">
+          <div className="grid grid-col-1 md:grid-cols-2  lg:grid-cols-3 gap-10">
             {members &&
               members.map((member) => {
                 return (
                   <div
                     key={member?.id}
-                    className="bg-white text-center h-[431px] flex flex-col items-center rounded-4xl membership-ranking-card p-5 md:p-0"
+                    className="bg-white text-center h-[431px] flex flex-col items-center justify-center  rounded-4xl membership-ranking-card px-5 pb-5"
                   >
-                    <Link href={member?.cardurl}>
+                    <Link href={member?.cardurl} target="_blank">
                       <Image
                         src={`${STRAPI_URL}${member.cardimg.url}`}
                         width={274}
                         height={274}
                         alt="acu"
+                        className="w-full"
                       />
                     </Link>
-                    <Link href={member?.cardurl} className="font-bold text-2xl">
+                    <Link href={member?.cardurl} className="font-semibold text-[22px] mt-5 max-w-[300px] w-full hover:text-[#0060aa]" target="_blank">
                       {member.cardtitle}
                     </Link>
                   </div>
@@ -45,7 +46,7 @@ const page = async () => {
         </div>
       </section>
       <section>
-        <div className="max-w-[1600px] mx-auto w-full mb-10 px-4 lg:px-0 text-center md:text-left">
+        <div className="max-w-[1600px] mx-auto w-full mb-10 px-4 text-center md:text-left">
           <h3 className="text-4xl lg:text-[64px] text-[#0944c8] font-bold mb-10">
             {membershipRanking?.rankingtitle}
           </h3>
---
+++ lib/api/accrediationrecogapproval.ts
@@ -0,0 +1,21 @@
+import { FETCH_STRAPI_URL } from "@/app/constant";
+import { MembershipAndRankingResponseType } from "../types/membership-and-ranking";
+import { AccrediationRecogApprovalsResponse } from "../types/accrecogapp";
+
+export async function getAccrediationRecognitionApprovalData(): Promise<
+  AccrediationRecogApprovalsResponse["data"]
+> {
+  const res = await fetch(
+    `${FETCH_STRAPI_URL}/api/accreditations-recognition-and-approval?populate[accrediationrecogapprovals][fields][0]=cardtitle&populate[accrediationrecogapprovals][fields][1]=cardurl&populate[accrediationrecogapprovals][populate][cardimg]=true`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+
+  if (!res.ok) throw new Error("Not Fetch Membership and Ranking Data");
+
+  const json: AccrediationRecogApprovalsResponse = await res.json();
+  return json.data;
+}
