5faccf2450196861d1cc553ad6e4a7de259f4c77
rbabucoder <baburohit0131@gmail.com>
2025-07-26 11:49:44 +0530

    fix the news and events design issue

--- app/Home/EventsAndNews.tsx
+++ app/Home/EventsAndNews.tsx
@@ -24,12 +24,12 @@ const EventsAndNews = async ({
 
   return (
     <section className="bg-[url(/homenewsevent.webp)] bg-cover bg-no-repeat px-5 pb-12 lg:px-8 lg:pb-20">
-      <div className="max-w-[1664px] mx-auto w-full">
-        <div className="lg:mx-8 ">
+      <div className="w-full">
+        <div>
           <h4 className="text-4xl lg:text-5xl leading-[1.17] font-light text-center text-white lg:pt-2.5 lg:pb-16 lg:text-left">
             {title}
           </h4>
-          <div className="grid gap-5 lg:gap-0 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 lg:px-0 mt-5 lg:mt-0">
+          <div className="grid gap-5 lg:gap-20 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 lg:px-0 mt-5 lg:mt-0">
             {newsandeventsdata.map((item) => (
               <HomeNewsEventsCard key={item.id} data={item} />
             ))}
