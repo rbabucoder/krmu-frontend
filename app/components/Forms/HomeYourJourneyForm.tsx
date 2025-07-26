df0f93fd9908c14bc8e320732dad96c0e88e87bb
rbabucoder <baburohit0131@gmail.com>
2025-07-26 12:02:32 +0530

    change placeholde

--- app/components/Forms/HomeYourJourneyForm.tsx
+++ app/components/Forms/HomeYourJourneyForm.tsx
@@ -4,7 +4,7 @@ const HomeYourJourneyForm = () => {
       <form>
         <input
           type="search"
-          placeholder="Search You Programmes"
+          placeholder="Search Your Programmes"
           className="text-center w-full bg-white text-black p-2.5 mb-2.5 rounded-md h-14 text-xl"
         />
         <input type="submit" value="Explore Our Programmes" className="py-1.5 px-5 bg-[#cb000d] text-white w-full text-center rounded-md font-bold" />
