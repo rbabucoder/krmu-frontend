b3b18bdd35069b80b23e0a12233dd7666860cc59
rbabucoder <baburohit0131@gmail.com>
2025-10-16 10:48:44 +0530

    fix

--- app/(schools)/[slug]/page.tsx
+++ app/(schools)/[slug]/page.tsx
@@ -49,6 +49,9 @@ export default async function Page({ params }: Props) {
   const degreeName = school?.degree?.name;
   const schoolCategoryName = school?.school_category?.name;
 
+  console.log('school', school);
+
+
   return (
     <>
       <SchoolHero
