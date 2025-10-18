607683c1d7d7fd61963641dc73eb369a4bb2f90f
rbabucoder <baburohit0131@gmail.com>
2025-10-18 13:18:05 +0530

    add live url

--- app/constant.ts
+++ app/constant.ts
@@ -1,4 +1,4 @@
-// export const STRAPI_URL =  "";
-// export const FETCH_STRAPI_URL = "https://truthful-cabbage-82fd27e8f6.strapiapp.com";
-export const STRAPI_URL = "http://localhost:1337";
-export const FETCH_STRAPI_URL = "http://localhost:1337";
+export const STRAPI_URL =  "";
+export const FETCH_STRAPI_URL = "https://truthful-cabbage-82fd27e8f6.strapiapp.com";
+// export const STRAPI_URL = "http://localhost:1337";
+// export const FETCH_STRAPI_URL = "http://localhost:1337";
