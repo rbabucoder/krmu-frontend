177c63b0a35851a7a67ba0fe6df7d2226a954335
= <=>
2025-11-22 11:19:36 +0530

    add read more or read less in student achievement card

--- app/(schools)/SchoolComponents/schoolglobalcomponent/StudentAchievementCard.tsx
+++ app/(schools)/SchoolComponents/schoolglobalcomponent/StudentAchievementCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { STRAPI_URL } from "@/app/constant";
 import {
   Carousel,
@@ -16,6 +19,13 @@ type Props = {
 };
 
 const StudentAchievementCard = ({ info, achievementsImages }: Props) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const limit = 150; // characters
+  const isLong = info?.length > limit;
+
+  const textToShow = readMore ? info : info?.slice(0, limit);
+
   return (
     <div className="border border-[#e2e2e2] p-5 rounded-[5px]">
       <div className="mb-5 space-y-4">
@@ -28,24 +38,23 @@ const StudentAchievementCard = ({ info, achievementsImages }: Props) => {
         >
           <CarouselContent>
             {achievementsImages &&
-              achievementsImages?.map((image, i) => {
-                return (
-                  <CarouselItem key={i}>
-                    {image?.url ? (
-                      <Image
-                        src={`${STRAPI_URL}${image?.url}`}
-                        width={443}
-                        height={476}
-                        alt=""
-                        className="h-80 w-full object-contain"
-                      />
-                    ) : (
-                      <Skeleton className="w-full h-80" />
-                    )}
-                  </CarouselItem>
-                );
-              })}
+              achievementsImages?.map((image, i) => (
+                <CarouselItem key={i}>
+                  {image?.url ? (
+                    <Image
+                      src={`${STRAPI_URL}${image?.url}`}
+                      width={443}
+                      height={476}
+                      alt=""
+                      className="h-80 w-full object-contain"
+                    />
+                  ) : (
+                    <Skeleton className="w-full h-80" />
+                  )}
+                </CarouselItem>
+              ))}
           </CarouselContent>
+
           {achievementsImages && achievementsImages.length > 1 && (
             <>
               <CarouselPrevious className="left-0 rounded-none text-white bg-black cursor-pointer" />
@@ -54,13 +63,23 @@ const StudentAchievementCard = ({ info, achievementsImages }: Props) => {
           )}
         </Carousel>
       </div>
+
       <div>
         <div
           dangerouslySetInnerHTML={{
-            __html: info,
+            __html: textToShow,
           }}
           className="student_achiev_content"
         />
+
+        {isLong && (
+          <button
+            className="text-red-600 cursor-pointer text-sm"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? "Read Less" : "Read More"}
+          </button>
+        )}
       </div>
     </div>
   );
