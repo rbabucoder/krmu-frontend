990f4f12be1d46905653d774c7c720a11778920d
= <=>
2025-11-22 17:17:56 +0530

    again change

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // output: "export",
   // basePath: "/blog3",
-  assetPrefix: "/blog2/",
+  assetPrefix: "/blog/",
   images: {
     remotePatterns: [
       {
