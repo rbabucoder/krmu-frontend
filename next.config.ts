0bf9f36f1f9116a62a507be9820d4ff466418953
= <=>
2025-11-22 16:44:19 +0530

    change in next config file add base path and prefix path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
+  basePath: "/blog3",
+  assetPrefix: "/blog3/",
   images: {
     remotePatterns: [
       {
