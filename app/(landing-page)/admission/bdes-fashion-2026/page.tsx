62441055d1dceb3309409139a4311f0a7617d3e4
= <=>
2025-12-26 10:26:37 +0530

    create bdes fashion game landing page

---
+++ app/(landing-page)/admission/bdes-fashion-2026/page.tsx
@@ -0,0 +1,114 @@
+import { Metadata } from "next";
+import ApplySection from "../CommonComponent2026/ApplyNow";
+import BusinessHub from "../CommonComponent2026/BusinessHub";
+import ComeToLife from "../CommonComponent2026/ComeToLife";
+import Counter from "../CommonComponent2026/Counter";
+import FAQ from "../CommonComponent2026/FAQ";
+import HeroSection from "../CommonComponent2026/HeroSection";
+import IndustryExperts from "../CommonComponent2026/IndustryExperts";
+import LifeAsPhDScholar from "../CommonComponent2026/LifeAsPhDScholar";
+import Opportunity from "../CommonComponent2026/Opportunity";
+import ProminentRecruiter from "../CommonComponent2026/ProminentRecruiter";
+// import ScholarResearchImpact from "../CommonComponent2026/ScholarResearchImpact";
+import Specialisation from "../CommonComponent2026/Specialisation";
+import Testimonials from "../CommonComponent2026/Testimonials";
+import WhyKRMangalam from "../CommonComponent2026/WhyKRMangalam";
+import { bdesFashionId } from "../constant/lpnpf";
+import FeeStructureTable from "./components/FeeStructure";
+import {
+  applySectionData,
+  businessHubData,
+  comeToLifeData,
+  Counters,
+  faqSectionData,
+  Hero,
+  heroMarqueeData,
+  industryExperts,
+  lifeAsPhDScholarData,
+  opportunityData,
+  prominentRecruiters,
+  // scholarResearchImpactData,
+  specialisationData,
+  testimonialsData,
+  whyKRM,
+} from "./content";
+
+import { SeoData } from "./seo";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: SeoData.title,
+    description: SeoData.description,
+
+    openGraph: {
+      title: SeoData.title,
+      description: SeoData.description,
+      images: [
+        {
+          url: SeoData.shareImage,
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: SeoData.title,
+      description: SeoData.description,
+      images: [SeoData.shareImage],
+    },
+  };
+}
+
+const page = () => {
+  const bdesFashionIdForm = bdesFashionId;
+  const heroContent = Hero;
+  const counters = Counters;
+  const recruiters = prominentRecruiters;
+  const whykrm = whyKRM;
+  const industryexpert = industryExperts;
+  const specialise = specialisationData;
+  const opport = opportunityData;
+  const testiomonial = testimonialsData;
+  // const scholarData = scholarResearchImpactData;
+  const worldLifeData = comeToLifeData;
+  const lifeData = lifeAsPhDScholarData;
+  const faqData = faqSectionData;
+  const businessData = businessHubData;
+  const applyData = applySectionData;
+  const heroMarqData = heroMarqueeData;
+
+  return (
+    <>
+      <HeroSection
+        content={heroContent?.title}
+        formId={bdesFashionIdForm}
+        desc={heroContent?.desc}
+        marqueeData={heroMarqData}
+      />
+      <Counter counters={counters} />
+      <ProminentRecruiter recruiters={recruiters} />
+      <WhyKRMangalam
+        heading={whykrm?.heading}
+        subheading={whykrm?.subheading}
+        desc={whykrm?.content}
+        imgUrl={whykrm?.imgUrl}
+        whykrmCards={whykrm?.whyKrmCards}
+      />
+      <IndustryExperts data={industryexpert} />
+      <Specialisation data={specialise} />
+      <Opportunity data={opport} />
+      <FeeStructureTable />
+      <Testimonials data={testiomonial} />
+      {/* <ScholarResearchImpact data={scholarData} /> */}
+      <ComeToLife data={worldLifeData} />
+      <LifeAsPhDScholar data={lifeData} />
+      <FAQ data={faqData} />
+      <BusinessHub data={businessData} formId={bdesFashionIdForm} />
+      <ApplySection data={applyData} />
+    </>
+  );
+};
+
+export default page;
--- app/(landing-page)/admission/constant/lpnpf.ts
+++ app/(landing-page)/admission/constant/lpnpf.ts
@@ -1,2 +1,4 @@
 export const lawFormId = "c0114ca224eb934f081f37dbc9fde07f";
 export const bbaFormId = "05a47f33fc1a48e7f6b357b1aaa542c7";
+export const bdesGameId = "582d52b6cdbd1a5596d68c362183bece";
+export const bdesFashionId = "582d52b6cdbd1a5596d68c362183bece";
