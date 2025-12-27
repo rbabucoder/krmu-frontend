29d8491189f8078667e30fcaa104ab2880efe938
= <=>
2025-12-27 12:42:18 +0530

    bsc agriculture, brand 2026, barch 2026, bsc agriculture, bse cse 2026

---
+++ app/(landing-page)/admission/aliied-health-science-2026/components/FeeStructure.tsx
@@ -0,0 +1,210 @@
+const FeeStructureTable = () => {
+  return (
+    <section className="w-full px-4 py-10">
+      <div className="max-w-[1400px] mx-auto w-full">
+        <h2 className="text-center text-2xl font-semibold mb-6">
+          Fee Structure
+        </h2>
+
+        <div className="overflow-x-auto rounded-xl border border-gray-200 feeStructTableContainer">
+          <table className="w-full">
+            <thead>
+              <tr className="bg-[#005da9] text-white text-left">
+                <th className="px-4 py-4 font-medium w-[40%]">Programmes</th>
+                <th className="px-4 py-4 font-medium w-[20%]">
+                  Semester-1 (In INR)
+                </th>
+                <th className="px-4 py-4 font-medium w-[20%]">
+                  Semester-2 (In INR)
+                </th>
+                <th className="px-4 py-4 font-medium w-[20%]">
+                  Annual Fees (In INR)
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="bg-white">
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">BBA (Human Resources)</td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">BBA (Marketing)</td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">BBA (Finance)</td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">BBA (International Business)</td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">BBA (Travel & Tourism)</td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Human Resources
+                </td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Marketing
+                </td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Finance
+                </td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) International Business
+                </td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Travel & Tourism
+                </td>
+                <td className="px-4 py-4">95,000</td>
+                <td className="px-4 py-4">90,000</td>
+                <td className="px-4 py-4">1,85,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Entrepreneurship) with academic support of GCEC Global
+                  Foundation
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Administration Entrepreneurship
+                  with academic support of GCEC Global Foundation
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Digital Marketing) with Academic Support of IIDE
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Digital Marketing with academic
+                  support of IIDE
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Business Analytics) with Academic Support of Ernst &
+                  Young (EY)
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">2,10,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Business Analytics with
+                  academic support of Ernst & Young (EY)
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">2,10,000</td>
+              </tr>
+
+              <tr className="text-sm">
+                <td className="px-4 py-4">
+                  BBA (International Accounting & Finance) (ACCA UK)
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) International Accounting &
+                  Finance (ACCA UK)
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+
+              <tr className="text-sm border-b">
+                <td className="px-4 py-4">
+                  BBA (Logistics and Supply Chain Management) with academic
+                  support of Safexpress
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+
+              <tr className="text-sm">
+                <td className="px-4 py-4">
+                  BBA (Hons./Hons with Research) Logistics and Supply Chain
+                  Management with academic support of Safexpress
+                </td>
+                <td className="px-4 py-4">1,05,000</td>
+                <td className="px-4 py-4">1,00,000</td>
+                <td className="px-4 py-4">2,05,000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeeStructureTable;
---
+++ app/(landing-page)/admission/aliied-health-science-2026/seo.ts
@@ -0,0 +1,6 @@
+export const SeoData = {
+  title: "Allied Health Science 2026 | K.R. Mangalam University",
+  description:
+    "Apply for LLB Admissions 2026 at K.R. Mangalam University.",
+  shareImage: "https://krmangalam.edu.in/og/law.jpg",
+};
