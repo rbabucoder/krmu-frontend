22a31285b2ac137921a4a9b430da6341abf4278a
= <=>
2026-01-10 15:34:21 +0530

    change domain

--- app/constant.ts
+++ app/constant.ts
@@ -3,7 +3,7 @@ export const FETCH_STRAPI_URL = "https://truthful-cabbage-82fd27e8f6.strapiapp.c
 // export const STRAPI_URL = "http://localhost:1337";
 // export const FETCH_STRAPI_URL = "http://localhost:1337";
 
-export const KRMUWordUrl = "https://www.krmangalam.edu.in";
+export const KRMUWordUrl = "https://wp.krmangalam.edu.in";
 // export const krmBlogURL = `${KRMUWordUrl}/blog2/blog-backend`;
 export const krmBlogURL = `${KRMUWordUrl}/blog`;
 // export const isLocal = STRAPI_URL.includes("localhost");
