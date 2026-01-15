ce45833c709b5dd0197d9302eac5c89ad1b02c5a
= <=>
2026-01-15 14:20:58 +0530

    fix this

--- app/constant.ts
+++ app/constant.ts
@@ -4,7 +4,7 @@ export const FETCH_STRAPI_URL = "https://truthful-cabbage-82fd27e8f6.strapiapp.c
 // export const FETCH_STRAPI_URL = "http://localhost:1337";
 // export const FETCH_STRAPI_URL = "http://techapi.krmangalam.edu.in";
 
-export const KRMUWordUrl = "https://krmangalam.edu.in";
+export const KRMUWordUrl = "https://wp.krmangalam.edu.in";
 // export const krmBlogURL = `${KRMUWordUrl}/blog2/blog-backend`; 
 export const krmBlogURL = `${KRMUWordUrl}/blog`;
 // export const isLocal = STRAPI_URL.includes("localhost");
